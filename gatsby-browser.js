c4fd1f12e5a5f4e1c308f156a9d04957f87d1a86
Zach Buttram <zach.buttram@gmail.com>
2020-10-17 19:19:08 -0700

    fix fontawesome css bug

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,6 @@
 import "./src/css/index.css"
+
+// Import font awesome's css and use it, otherwise it injects its styles via JS at runtime
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import { config } from "@fortawesome/fontawesome-svg-core"
+config.autoAddCss = false
